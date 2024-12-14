11fcbc875e857048a19549f5125b4b2718fe37b2
Seio924 <seio9241@gmail.com>
2024-12-14 21:06:01 +0900

    feat(#8): 인증 코드 검증 컴포넌트 추가

---
+++ src/components/EmailVerifyField/PinCodeInput/PinCodeInput.styles.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/native';
+
+const Wrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  margin: 6px 0 15px 0;
+`;
+
+const PinInput = styled.TextInput`
+  width: 49;
+  height: 56;
+  border: 1px solid #717d96;
+  border-radius: 17px;
+`;
+
+const S = {
+  Wrapper,
+  PinInput,
+};
+
+export default S;
---
+++ src/components/EmailVerifyField/PinCodeInput/PinCodeInput.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import S from './PinCodeInput.styles';
+
+interface PinCodeInputProps {
+  pinLength: number;
+  setVerifyCode?: (code: string) => void;
+}
+
+export default function PinCodeInput({
+  pinLength,
+  setVerifyCode,
+}: PinCodeInputProps) {
+  const [pinValues, setPinValues] = useState(Array(pinLength).fill(''));
+  const inputRefs = useRef<TextInput[]>([]);
+
+  const handleChange = (index: number, value: string) => {
+    const newPinValues = [...pinValues];
+    newPinValues[index] = value;
+    setPinValues(newPinValues);
+
+    if (value && index < pinLength - 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
+  const handleBackspace = (index: number, value: string) => {
+    if (value === '' && index > 0) {
+      const newPinValues = [...pinValues];
+      newPinValues[index] = '';
+      setPinValues(newPinValues);
+
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
+
+  return (
+    <S.Wrapper>
+      {pinValues.map((value, index) => (
+        <S.PinInput
+          key={index}
+          keyboardType='numeric'
+          maxLength={1}
+          value={value}
+          onChangeText={text => handleChange(index, text)}
+          onKeyPress={({ nativeEvent }) => {
+            if (nativeEvent.key === 'Backspace') {
+              handleBackspace(index, value);
+            }
+          }}
+          ref={el => {
+            if (el) inputRefs.current[index] = el;
+          }}
+          autoFocus={index === 0}
+        />
+      ))}
+    </S.Wrapper>
+  );
+}
