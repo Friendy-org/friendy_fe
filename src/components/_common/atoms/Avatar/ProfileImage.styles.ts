81ef751b7196032e8f6a9d6c7015b3e341491100
Seio924 <seio9241@gmail.com>
2024-12-15 15:15:55 +0900

    feat(#8): 프로필 이미지 설정 컴포넌트 추가

---
+++ src/components/_common/atoms/Avatar/ProfileImage.styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/native';
+
+const ProfileImage = styled.Image`
+  width: 104px;
+  height: 104px;
+  border-radius: 104px;
+`;
+
+const S = {
+  ProfileImage,
+};
+
+export default S;
