e8cbc0b62c566de1ce555b4b166a6593d820d9af
Seio924 <seio9241@gmail.com>
2024-12-18 20:43:29 +0900

    feat(#8): 유효성 검사 로직 추가

--- src/screens/SignUpScreen/SignUpScreen.tsx
+++ src/screens/SignUpScreen/SignUpScreen.tsx
@@ -25,8 +25,47 @@ export default function SignUpScreen({ navigation }: SignUpScreenProps) {
   const [date, setDate] = useState(new Date());
   const [step, setStep] = useState(1);
 
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      throw new Error('이메일을 확인해 주세요.');
+    }
+  };
+
+  const validateNickname = (nickname: string) => {
+    if (nickname.length < 8 || nickname.length > 30) {
+      throw new Error('닉네임 길이가 맞지 않습니다.');
+    }
+  };
+
+  const validatePassword = (password: string) => {
+    if (password.length < 8 || password.length > 16) {
+      throw new Error('비밀번호 길이가 맞지 않습니다.');
+    }
+  };
+
+  const onSubmitStep1 = (data: typeof formData) => {
+    console.log('Step 1 data:', data);
+    setStep(2);
+  };
+
+  const onSubmitStep2 = (data: typeof formData) => {
+    console.log('Step 2 data:', data);
+    setStep(3);
+  };
+
+  const onSubmitStep3 = (data: typeof formData) => {
+    console.log('Final Step data:', data);
+  };
+
   const handlePress = () => {
-    setStep(step + 1);
+    if (step === 1) {
+      handleSubmit(onSubmitStep1);
+    } else if (step === 2) {
+      handleSubmit(onSubmitStep2);
+    } else if (step === 3) {
+      handleSubmit(onSubmitStep3);
+    }
   };
 
   return (
@@ -34,7 +73,10 @@ export default function SignUpScreen({ navigation }: SignUpScreenProps) {
       <S.SignUpContainer>
         <S.Title>회원가입</S.Title>
         {step === 1 && (
-          <InputField label='이메일을 입력해 주세요.' {...register('email')} />
+          <InputField
+            label='이메일을 입력해 주세요.'
+            {...register('email', { validate: validateEmail })}
+          />
         )}
 
         {step === 2 && <EmailVerifyField email={formData.email} />}
@@ -42,11 +84,17 @@ export default function SignUpScreen({ navigation }: SignUpScreenProps) {
         {step === 3 && (
           <S.InnerForm>
             <ChangeProfileImageField buttonText='프로필 사진 선택' />
-            <InputField label='닉네임' {...register('nickname')} />
-            <PasswordInput label='비밀번호' {...register('password')} />
+            <InputField
+              label='닉네임'
+              {...register('nickname', { validate: validateNickname })}
+            />
+            <PasswordInput
+              label='비밀번호'
+              {...register('password', { validate: validatePassword })}
+            />
             <PasswordInput
               label='비밀번호 확인'
-              {...register('confirmPassword')}
+              {...register('confirmPassword', { validate: validatePassword })}
             />
             <DateInputField selectedDate={date} onChangeDate={setDate} />
           </S.InnerForm>
