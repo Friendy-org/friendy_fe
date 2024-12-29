e796a601df749327c77d70f649e73ae3d7a7568a
Wooki <hyunook02@naver.com>
2024-12-29 21:09:23 +0900

    feat(#10): 초기 화면 추가

--- src/components/EmailVerification/EmailVerificationStep2/EmailVerificationStep2.tsx
+++ src/components/EmailVerification/EmailVerificationStep2/EmailVerificationStep2.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PinCodeInput from '../../_common/molecules/PinCodeInput/PinCodeInput';
 import S from './EmailVerificationStep2.styles';
 import Button from '../../_common/atoms/Button/Button';
+import LinkedText from '../../_common/atoms/LinkedText/LinkedText';
 
 interface EmailVerificationStep2Props {
   email: string;
@@ -14,8 +15,13 @@ export default function EmailVerificationStep2({ email, nextStep }: EmailVerific
     console.log('Step 2 complete');
   };
 
-  const handlePress = () => {
-    handleStep2Submit();
+  const handleResend = () => {
+    // todo: 이메일 재전송 api
+    console.log('Resend email');
+  }
+
+  const handlePress = (func: any) => {
+    func();
     nextStep();
   };
 
@@ -25,11 +31,11 @@ export default function EmailVerificationStep2({ email, nextStep }: EmailVerific
         <S.Label>{email}으로 전송된 인증 코드 6자리를 입력해 주세요.</S.Label>
         <PinCodeInput pinLength={6} />
         <S.InfoWrapper>
-          <S.ResendLink>인증 코드가 오지 않나요?</S.ResendLink>
+          <LinkedText onPress={() => {handlePress(handleResend)}}>인증 코드가 오지 않나요?</LinkedText>
           <S.Timer>5:00</S.Timer>
         </S.InfoWrapper>
       </S.VerificationContainer>
-      <Button onPress={handlePress}>다음</Button>
+      <Button onPress={() => {handlePress(handleStep2Submit)}}>다음</Button>
     </>
   );
 }
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SignUpScreen from '../screens/SignUpScreen/SignUpScreen';
+import RootScreen from '../screens/RootScreen/RootScreen';
 import { RootStackParamList } from '../types/NavigationTypes';
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -10,9 +11,10 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='SignUp'
+        initialRouteName='Root'
         screenOptions={{ headerShown: false }}
       >
+        <Stack.Screen name='Root' component={RootScreen} />
         <Stack.Screen name='SignUp' component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
