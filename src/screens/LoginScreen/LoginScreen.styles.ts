7b2a9a3d046f298d06236eb507d1dc44a2c54a7b
Wooki <hyunook02@naver.com>
2025-01-03 02:49:30 +0900

    feat(#10): LoginScreen 페이지 레이아웃 작성

---
+++ src/screens/LoginScreen/LoginScreen.styles.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/native';
+
+const Layout = styled.View`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    height: 80%;
+    margin: auto;
+    justify-content: space-between;
+    align-items: start;
+`
+
+const title = styled.Text`
+    margin: 0px 10px;
+    font-size: 24px;
+`;
+
+const Section = styled.View`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const S = {
+    Layout,
+    title,
+    Section
+}
+
+export default S;
