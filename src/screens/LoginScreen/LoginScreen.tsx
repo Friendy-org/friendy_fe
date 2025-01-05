ec7818a8cbf1a8eca3c41e583f3d3e021491052b
Wooki <hyunook02@naver.com>
2025-01-06 03:30:15 +0900

    feat(#10): 로그인 창 텍스트필드 유효성 검사

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -5,17 +5,31 @@ import InputField from '../../components/_common/molecules/InputField/InputField
 import PasswordInput from '../../components/PasswordInput/PasswordInput';
 import LinkedText from '../../components/_common/atoms/LinkedText/LinkedText';
 import { RootScreenProps } from "../RootScreen/RootScreen";
+import useForm from '../../hooks/utils/useForm';
+import { validateNull } from '../../validations/userValidators';
 
 export default function LoginScreen({ navigation }: RootScreenProps) {
 
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const { formData, register, handleSubmit } = useForm({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+    });
 
+    const handlePress = (func: any) => {
+        const hasError = handleSubmit(async () => await func());
+        if (!hasError) {
+            // Todo: navigate to main screen
+            navigation.navigate({ name: 'Root', params: undefined });
+        }
+    }
     const handleLogin = () => {
-        // Todo: Implement login logic
+        // Todo: login API
+        console.log('Login complete');
     }
     const handleForgotPassword = () => {
-        // Todo: Implement forgot password logic
+        // Todo: navigate to forgot password screen
     }
 
     return (
@@ -23,20 +37,18 @@ export default function LoginScreen({ navigation }: RootScreenProps) {
             <S.title>로그인</S.title>
             <S.Section>
                 <InputField 
-                    label={'이메일'} 
-                    value={email}
-                    onChange={function (text: string): void { setEmail(text); }}
+                    label={'이메일'}
                     marginBottom={14}
+                    {...register('email', {validate: validateNull})}
                 ></InputField>
                 <PasswordInput
                     label={'비밀번호'}
-                    value={password}
-                    onChange={function (text: string): void { setPassword(text); }}
                     marginBottom={30}
+                    {...register('password', {validate: validateNull})}
                 ></PasswordInput>
                 <LinkedText onPress={handleForgotPassword}>비밀번호가 기억나지 않나요?</LinkedText>
             </S.Section>
-            <Button onPress={handleLogin}>완료</Button>
+            <Button onPress={() => {handlePress(handleLogin)}}>완료</Button>
         </S.Layout>
     );
 };
