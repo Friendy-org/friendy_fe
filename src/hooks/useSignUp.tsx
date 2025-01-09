b74a8429966f8bc4aafee14870bffabe7f5caf84
Seio924 <seio9241@gmail.com>
2025-01-09 23:26:08 +0900

    feat(#11): 회원가입 훅 구현
    refactor: LoginScreen 줄바꿈 추가

---
+++ src/hooks/useSignUp.tsx
@@ -0,0 +1,36 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigation } from '@react-navigation/native';
+import signUp from '../api/domain/member';
+import { RootStackParamList } from '../types/NavigationTypes';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type NavigationProp = StackNavigationProp<RootStackParamList>;
+
+export default function useSignUp() {
+  const navigation = useNavigation<NavigationProp>();
+
+  const signUpMutate = useMutation({
+    mutationFn: ({
+      email,
+      nickname,
+      password,
+      birthDate,
+    }: {
+      email: string;
+      nickname: string;
+      password: string;
+      birthDate: string;
+    }) =>
+      signUp({
+        email,
+        nickname,
+        password,
+        birthDate,
+      }),
+    onSuccess: () => {
+      navigation.navigate('Login');
+    },
+  });
+
+  return { signUpMutate };
+}
