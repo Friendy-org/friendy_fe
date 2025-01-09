61732b6ef928e23626644de7b4d5ffada507e438
Seio924 <seio9241@gmail.com>
2025-01-09 21:46:46 +0900

    fix(#11): navigate 오류 해결

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,55 +1,63 @@
-import React from 'react'
-import S from "./LoginScreen.styles";
+import React from 'react';
+import S from './LoginScreen.styles';
 import Button from '../../components/_common/atoms/Button/Button';
 import InputField from '../../components/_common/molecules/InputField/InputField';
 import PasswordInput from '../../components/PasswordInput/PasswordInput';
 import LinkedText from '../../components/_common/atoms/LinkedText/LinkedText';
-import { RootScreenProps } from "../RootScreen/RootScreen";
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types/NavigationTypes';
 import useForm from '../../hooks/utils/useForm';
 import { validateNull } from '../../validations/userValidators';
 
-export default function LoginScreen({ navigation }: RootScreenProps) {
+export type LoginScreenProps = StackScreenProps<RootStackParamList, 'Login'>;
 
-    const { formData, register, handleSubmit } = useForm({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-    });
+export default function LoginScreen({ navigation }: LoginScreenProps) {
+  const { formData, register, handleSubmit } = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  });
 
-    const handlePress = (func: any) => {
-        const hasError = handleSubmit(async () => await func());
-        if (!hasError) {
-            // Todo: navigate to main screen
-            navigation.navigate({ name: 'Root', params: undefined });
-        }
+  const handlePress = (func: any) => {
+    const hasError = handleSubmit(async () => await func());
+    if (!hasError) {
+      // Todo: navigate to main screen
+      navigation.navigate({ name: 'Root', params: undefined });
     }
-    const handleLogin = () => {
-        // Todo: login API
-        console.log('Login complete');
-    }
-    const handleForgotPassword = () => {
-        // Todo: navigate to forgot password screen
-    }
+  };
+  const handleLogin = () => {
+    // Todo: login API
+    console.log('Login complete');
+  };
+  const handleForgotPassword = () => {
+    // Todo: navigate to forgot password screen
+  };
 
-    return (
-        <S.AppContainer>
-            <S.Layout>
-                <S.Section>
-                    <InputField 
-                        label={'이메일'}
-                        marginBottom={14}
-                        {...register('email', {validate: validateNull})}
-                    ></InputField>
-                    <PasswordInput
-                        label={'비밀번호'}
-                        marginBottom={30}
-                        {...register('password', {validate: validateNull})}
-                    ></PasswordInput>
-                    <LinkedText onPress={handleForgotPassword}>비밀번호가 기억나지 않나요?</LinkedText>
-                </S.Section>
-                <Button onPress={() => {handlePress(handleLogin)}}>완료</Button>
-            </S.Layout>
-        </S.AppContainer>
-    );
-};
+  return (
+    <S.AppContainer>
+      <S.Layout>
+        <S.Section>
+          <InputField
+            label={'이메일'}
+            marginBottom={14}
+            {...register('email', { validate: validateNull })}
+          ></InputField>
+          <PasswordInput
+            label={'비밀번호'}
+            marginBottom={30}
+            {...register('password', { validate: validateNull })}
+          ></PasswordInput>
+          <LinkedText onPress={handleForgotPassword}>비밀번호가 기억나지 않나요?</LinkedText>
+        </S.Section>
+        <Button
+          onPress={() => {
+            handlePress(handleLogin);
+          }}
+        >
+          완료
+        </Button>
+      </S.Layout>
+    </S.AppContainer>
+  );
+}
