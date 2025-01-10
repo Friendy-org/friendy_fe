2dcac1cee4adc3ed9f0fba25c262cba3fdce02c9
Seio924 <seio9241@gmail.com>
2025-01-10 15:50:09 +0900

    refactor(#11): 유효성 검사 에러 메시지 상수화

--- src/validations/userValidators.ts
+++ src/validations/userValidators.ts
@@ -1,21 +1,23 @@
 // todo: 유효성 검사 상세하게
 
+import { FORM_ERROR } from '../constants/errorMessages';
+
 export const validateEmail = (email: string) => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailRegex.test(email)) {
-    throw new Error('이메일을 확인해 주세요.');
+    throw new Error(FORM_ERROR.EMAIL.INVALID);
   }
 };
 
 export const validateNickname = (nickname: string) => {
   if (nickname.length < 8 || nickname.length > 30) {
-    throw new Error('닉네임 길이가 맞지 않습니다.');
+    throw new Error(FORM_ERROR.NICKNAME.LENGTH);
   }
 };
 
 export const validatePassword = (password: string) => {
   if (password.length < 8 || password.length > 16) {
-    throw new Error('비밀번호 길이가 맞지 않습니다.');
+    throw new Error(FORM_ERROR.PASSWORD.LENGTH);
   }
 };
 
