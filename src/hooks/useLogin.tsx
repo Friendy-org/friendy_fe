2fd0562fe4c8e311b11b21c412b99ef0e2e86f9e
Wooki <hyunook02@naver.com>
2025-01-11 18:43:28 +0900

    feat(#15): useLogin 훅 구현

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,32 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../types/NavigationTypes";
+import { useNavigation } from "@react-navigation/native";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "../api/domain/member";
+
+type NavigationProp = StackNavigationProp<RootStackParamList>;
+
+export default function useLogin() {
+  const navigation = useNavigation<NavigationProp>();
+
+  const loginMutate = useMutation({
+    mutationFn: ({
+      email,
+      password
+    }: {
+      email: string;
+      password: string
+    }) =>
+      login({
+        email,
+        password
+      }),
+    onSuccess: () => {
+      console.log("로그인 성공");
+      // Todo: navigate to Home
+      navigation.navigate("Root");
+    },
+  });
+
+  return { loginMutate };
+}
