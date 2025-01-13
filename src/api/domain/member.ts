dec3c8c5142dad2f0d4a56fe39bd12ed3867bf7f
Seio924 <seio9241@gmail.com>
2025-01-13 17:58:13 +0900

    refactor(#18): auth, member API 관리 통합

--- src/api/domain/member.ts
+++ src/api/domain/member.ts
@@ -2,26 +2,27 @@ import ApiClient from '../ApiClient';
 
 const apiClient = new ApiClient('');
 
-const signUp = async ({
-  email,
-  nickname,
-  password,
-  birthDate,
-}: {
-  email: string;
-  nickname: string;
-  password: string;
-  birthDate: string;
-}) => {
-  await apiClient.post({
-    path: '/signup',
-    body: {
-      email,
-      nickname,
-      password,
-      birthDate,
-    },
-  });
+const memberApis = {
+  signUp: async ({
+    email,
+    nickname,
+    password,
+    birthDate,
+  }: {
+    email: string;
+    nickname: string;
+    password: string;
+    birthDate: string;
+  }) =>
+    await apiClient.post({
+      path: '/signup',
+      body: {
+        email,
+        nickname,
+        password,
+        birthDate,
+      },
+    }),
 };
 
-export default signUp;
+export default memberApis;
