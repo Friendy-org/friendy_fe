88f12c5053bc13fe504154e1b53e700cf5d4bea2
Seio924 <seio9241@gmail.com>
2025-01-13 15:36:15 +0900

    refactor(#18): 이메일 관련 API 관리 통합

--- src/api/domain/email.ts
+++ src/api/domain/email.ts
@@ -2,13 +2,12 @@ import ApiClient from '../ApiClient';
 
 const apiClient = new ApiClient('/email');
 
-const sendEmail = async ({ email }: { email: string }) => {
-  await apiClient.post({
-    path: '/send-code',
-    body: {
-      email,
-    },
-  });
+const emailApis = {
+  sendCode: async ({ email }: { email: string }) =>
+    await apiClient.post({
+      path: '/send-code',
+      body: { email },
+    }),
 };
 
-export default sendEmail;
+export default emailApis;
