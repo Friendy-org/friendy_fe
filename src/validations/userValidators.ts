df5bb3c58b2e942d3978e136cd63380228fc1437
Seio924 <seio9241@gmail.com>
2025-01-17 01:16:30 +0900

    feat(#21): 유효성 검사 수정

--- src/validations/userValidators.ts
+++ src/validations/userValidators.ts
@@ -1,8 +1,8 @@
-// todo: 유효성 검사 상세하게
-
 import { FORM_ERROR } from '../constants/errorMessages';
 
 export const validateEmail = (email: string) => {
+  validateNull(email);
+
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailRegex.test(email)) {
     throw new Error(FORM_ERROR.EMAIL.INVALID);
@@ -10,14 +10,23 @@ export const validateEmail = (email: string) => {
 };
 
 export const validateNickname = (nickname: string) => {
-  if (nickname.length < 8 || nickname.length > 30) {
+  validateNull(nickname);
+
+  if (nickname.length < 2 || nickname.length > 20) {
     throw new Error(FORM_ERROR.NICKNAME.LENGTH);
   }
 };
 
 export const validatePassword = (password: string) => {
+  validateNull(password);
+
   if (password.length < 8 || password.length > 16) {
     throw new Error(FORM_ERROR.PASSWORD.LENGTH);
+  }
+
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*?]).*$/;
+  if (!passwordRegex.test(password)) {
+    throw new Error(FORM_ERROR.PASSWORD.INVALID);
   }
 };
 
