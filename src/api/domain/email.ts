d6fca738ae6e3ed88c11451c461f67f65dd02fa0
Seio924 <seio9241@gmail.com>
2025-01-16 21:23:10 +0900

    feat(#21): 회원가입, 이메일 API 요청 시 토큰 제외하도록 변경

--- src/api/domain/email.ts
+++ src/api/domain/email.ts
@@ -7,12 +7,14 @@ const emailApis = {
     await apiClient.post({
       path: '/send-code',
       body: { email },
+      requiresAuth: false,
     }),
 
   verifyCode: async ({ email, authCode }: { email: string; authCode: string }) =>
     await apiClient.post({
       path: '/verify-code',
       body: { email, authCode },
+      requiresAuth: false,
     }),
 };
 
