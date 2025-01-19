3c10376161f7a346810082913ae839a43ee25e5a
Seio924 <seio9241@gmail.com>
2025-01-19 12:57:28 +0900

    feat(#29): navigation 타입 설정

--- src/types/NavigationTypes.ts
+++ src/types/NavigationTypes.ts
@@ -3,4 +3,13 @@ export type RootStackParamList = {
   Login: undefined;
   SignUp: undefined;
   ResetPassword: undefined;
+  Main: undefined;
+};
+
+export type TabParamList = {
+  Explore: undefined;
+  Feed: undefined;
+  Map: undefined;
+  DM: undefined;
+  Profile: undefined;
 };
