a99c004fe2413c9bf113d3044d65e41d17215bfc
Seio924 <seio9241@gmail.com>
2025-01-20 00:23:41 +0900

    feat(#29): HeaderBar 수정 및 Map, Feed에 추가

--- src/screens/FeedScreen/index.tsx
+++ src/screens/FeedScreen/index.tsx
@@ -1,9 +1,11 @@
+import HeaderBar from '@navigation/HeaderBar';
 import MainLayout from '@screens/MainLayout';
 import { Text } from 'react-native';
 
 export default function FeedScreen() {
   return (
     <MainLayout location='Feed'>
+      <HeaderBar showEditButton={true} />
       <Text>FeedScreen</Text>
     </MainLayout>
   );
