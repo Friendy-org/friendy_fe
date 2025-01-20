bea5ebb534c92d8c72b43388018889893158fd1e
Seio924 <seio9241@gmail.com>
2025-01-21 02:42:40 +0900

    feat(#31): 프로필 이미지 props로 추가

--- src/components/_common/molecules/AuthorInfo/index.tsx
+++ src/components/_common/molecules/AuthorInfo/index.tsx
@@ -7,10 +7,14 @@ interface AuthorInfoProps {
   location: string;
 }
 
-export default function AuthorInfo({ profileImageUrl, name, location }: AuthorInfoProps) {
+export default function AuthorInfo({
+  profileImageUrl = 'https://i.namu.wiki/i/w7GkIKr6Qac-0SCYEn7DdYBpkpZed9FaVNTBFE7aIQvm7p39bo7gs2Pb1ZWfX3dPVd0JmA3oX50T5kl-MU7wfw.webp',
+  name,
+  location,
+}: AuthorInfoProps) {
   return (
     <S.Container>
-      <S.ProfileImage />
+      <S.ProfileImage source={{ uri: profileImageUrl }} />
       <S.InfoWrapper>
         <S.Name>{name}</S.Name>
         <S.Location>{location}</S.Location>
