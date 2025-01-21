549954dff338c0635fe7638905445b728e05e544
Seio924 <seio9241@gmail.com>
2025-01-21 15:22:07 +0900

    feat(#33): 게시글 생성 화면 navigate 설정

---
+++ src/screens/FeedCreateScreen/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@screens/MainLayout';
+import { Text } from 'react-native';
+
+export default function FeedCreateScreen() {
+  return (
+    <MainLayout location='FeedCreate'>
+      <Text>FeedCreateScreen</Text>
+    </MainLayout>
+  );
+}
