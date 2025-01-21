a126c1effc3091b72fb6538ad3637aaab27c49d9
Seio924 <seio9241@gmail.com>
2025-01-21 18:09:39 +0900

    feat(#33): ImageUploader 컴포넌트 추가

---
+++ src/components/ImageUploader/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
+import * as ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/Feather';
+import S from './style';
+
+export default function ImageUploader() {
+  const [images, setImages] = useState<string[]>([]);
+
+  const selectImage = () => {
+    ImagePicker.launchImageLibrary({ mediaType: 'photo', quality: 1 }, response => {
+      if (response.assets && response.assets[0]?.uri) {
+        setImages([...images, response.assets[0].uri]);
+      }
+    });
+  };
+
+  const removeImage = (index: number) => {
+    const updatedImages = images.filter((_, i) => i !== index);
+    setImages(updatedImages);
+  };
+
+  return (
+    <S.Container hasImages={images.length > 0}>
+      <FlatList
+        data={[...images, null]}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) =>
+          item ? (
+            <S.ImageWrapper key={index}>
+              <S.UploadImage source={{ uri: item }} />
+              <S.CloseButton onPress={() => removeImage(index)}>
+                <Icon name='x' size={16} color='#000' />
+              </S.CloseButton>
+            </S.ImageWrapper>
+          ) : (
+            <S.AddImageButton onPress={selectImage}>
+              <Icon name='plus' size={32} color='#000' />
+            </S.AddImageButton>
+          )
+        }
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </S.Container>
+  );
+}
--- src/screens/FeedCreateScreen/index.tsx
+++ src/screens/FeedCreateScreen/index.tsx
@@ -1,10 +1,11 @@
+import ImageUploader from '@components/ImageUploader';
 import MainLayout from '@screens/MainLayout';
 import { Text } from 'react-native';
 
 export default function FeedCreateScreen() {
   return (
     <MainLayout location='FeedCreate'>
-      <Text>FeedCreateScreen</Text>
+      <ImageUploader />
     </MainLayout>
   );
 }
