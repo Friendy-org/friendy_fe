207411405f7cfa5b418280d6b44f93e316f5b7e4
Seio924 <seio9241@gmail.com>
2025-01-22 23:16:44 +0900

    feat(#33): FeedCreateScreen에 버튼 추가

--- src/screens/FeedCreateScreen/style.ts
+++ src/screens/FeedCreateScreen/style.ts
@@ -1,11 +1,15 @@
 import styled from '@emotion/native';
 
 const Layout = styled.View`
+  display: flex;
+  justify-content: space-between;
   padding: 40px 0px;
   background-color: #fff;
   width: 100%;
   height: 100%;
 `;
+
+const FeedCreateContainer = styled.View``;
 
 const ImageForm = styled.View<{ hasImages: boolean }>`
   padding-right: 30px;
@@ -28,12 +32,18 @@ const ContentLabel = styled.Text`
   margin-bottom: 5px;
 `;
 
+const ButtonWrapper = styled.View`
+  padding: 0 30px;
+`;
+
 const S = {
+  FeedCreateContainer,
   Layout,
   ImageForm,
   ImageLabel,
   ContentWrapper,
   ContentLabel,
+  ButtonWrapper,
 };
 
 export default S;
