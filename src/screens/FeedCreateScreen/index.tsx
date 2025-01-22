f0bc3c8bd05e13336bb0cf11861e268e8383dea1
Seio924 <seio9241@gmail.com>
2025-01-22 23:16:44 +0900

    feat(#33): FeedCreateScreen에 버튼 추가

--- src/screens/FeedCreateScreen/index.tsx
+++ src/screens/FeedCreateScreen/index.tsx
@@ -2,20 +2,28 @@ import ImageUploader from '@components/ImageUploader';
 import S from './style';
 import { useState } from 'react';
 import ContentInput from '@components/ContentInput';
+import Button from '@components/_common/atoms/Button';
 
 export default function FeedCreateScreen() {
   const [images, setImages] = useState<string[]>([]);
 
+  const handlePress = () => {};
+
   return (
     <S.Layout>
-      <S.ImageLabel>선택한 이미지</S.ImageLabel>
-      <S.ImageForm hasImages={images.length > 0}>
-        <ImageUploader images={images} setImages={setImages} />
-      </S.ImageForm>
-      <S.ContentWrapper>
-        <S.ContentLabel>본문</S.ContentLabel>
-        <ContentInput />
-      </S.ContentWrapper>
+      <S.FeedCreateContainer>
+        <S.ImageLabel>선택한 이미지</S.ImageLabel>
+        <S.ImageForm hasImages={images.length > 0}>
+          <ImageUploader images={images} setImages={setImages} />
+        </S.ImageForm>
+        <S.ContentWrapper>
+          <S.ContentLabel>본문</S.ContentLabel>
+          <ContentInput />
+        </S.ContentWrapper>
+      </S.FeedCreateContainer>
+      <S.ButtonWrapper>
+        <Button onPress={handlePress}>완료</Button>
+      </S.ButtonWrapper>
     </S.Layout>
   );
 }
