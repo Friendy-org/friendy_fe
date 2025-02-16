21371954e831750cab9b2fdedf5e100a70d9b817
Seio924 <seio9241@gmail.com>
2025-02-16 14:31:24 +0900

    feat(#40): 디자인 시스템 폰트 설정

---
+++ src/styles/font.ts
@@ -0,0 +1,91 @@
+import { css } from '@emotion/react';
+
+const typography = {
+  heading: {
+    900: css`
+      font-weight: 700;
+      font-size: 32;
+      letter-spacing: 0;
+    `,
+    800: css`
+      font-weight: 700;
+      font-size: 30;
+      letter-spacing: 0;
+    `,
+    700: css`
+      font-weight: 700;
+      font-size: 28;
+      letter-spacing: 0;
+    `,
+    600: css`
+      font-weight: 700;
+      font-size: 26;
+      letter-spacing: 0;
+    `,
+    500: css`
+      font-weight: 700;
+      font-size: 24;
+      letter-spacing: 0;
+    `,
+    400: css`
+      font-weight: 700;
+      font-size: 22;
+      letter-spacing: 0;
+    `,
+    300: css`
+      font-weight: 700;
+      font-size: 20;
+      letter-spacing: 0;
+    `,
+    200: css`
+      font-weight: 700;
+      font-size: 18;
+      letter-spacing: 0;
+    `,
+    100: css`
+      font-weight: 700;
+      font-size: 16;
+      letter-spacing: 0;
+    `,
+  },
+
+  common: {
+    large: css`
+      font-weight: 500;
+      font-size: 16;
+      letter-spacing: 0;
+    `,
+    medium: css`
+      font-weight: 400;
+      font-size: 14;
+      letter-spacing: 0;
+    `,
+    regular: css`
+      font-weight: 500;
+      font-size: 12;
+      letter-spacing: 0;
+    `,
+    regularAccent: css`
+      font-weight: 700;
+      font-size: 12;
+      letter-spacing: 0;
+    `,
+    small: css`
+      font-weight: 400;
+      font-size: 10;
+      letter-spacing: 0;
+    `,
+    smallAccent: css`
+      font-weight: 600;
+      font-size: 10;
+      letter-spacing: 0;
+    `,
+    extraSmall: css`
+      font-weight: 400;
+      font-size: 8;
+      letter-spacing: 0;
+    `,
+  },
+};
+
+export default typography;
