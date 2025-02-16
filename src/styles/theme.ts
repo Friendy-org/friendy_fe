c1e461c26b593428f4d7c95018d79b40d4d240a0
Seio924 <seio9241@gmail.com>
2025-02-16 14:31:24 +0900

    feat(#40): 디자인 시스템 폰트 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,11 @@
 import { Theme } from '@emotion/react';
-import { dark, light } from '@styles/color';
+import { dark, light } from './color';
+import typography from './font';
 
 const mode: 'light' | 'dark' = 'light';
 
 export const theme: Theme = {
   mode,
   color: mode === 'light' ? light : dark,
+  font: typography,
 };
