9c4eda0a10bdb4e229c696662c052dae75534167
Seio924 <seio9241@gmail.com>
2025-02-16 19:59:53 +0900

    refactor(#40): px 단위 추가

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -2,47 +2,47 @@ const typography = {
   heading: {
     900: `
       font-weight: 700;
-      font-size: 32;
+      font-size: 32px;
       letter-spacing: 0;
     `,
     800: `
       font-weight: 700;
-      font-size: 30;
+      font-size: 30px;
       letter-spacing: 0;
     `,
     700: `
       font-weight: 700;
-      font-size: 28;
+      font-size: 28px;
       letter-spacing: 0;
     `,
     600: `
       font-weight: 700;
-      font-size: 26;
+      font-size: 26px;
       letter-spacing: 0;
     `,
     500: `
       font-weight: 700;
-      font-size: 24;
+      font-size: 24px;
       letter-spacing: 0;
     `,
     400: `
       font-weight: 700;
-      font-size: 22;
+      font-size: 22px;
       letter-spacing: 0;
     `,
     300: `
       font-weight: 700;
-      font-size: 20;
+      font-size: 20px;
       letter-spacing: 0;
     `,
     200: `
       font-weight: 700;
-      font-size: 18;
+      font-size: 18px;
       letter-spacing: 0;
     `,
     100: `
       font-weight: 700;
-      font-size: 16;
+      font-size: 16px;
       letter-spacing: 0;
     `,
   },
@@ -50,37 +50,37 @@ const typography = {
   common: {
     large: `
       font-weight: 500;
-      font-size: 16;
+      font-size: 16px;
       letter-spacing: 0;
     `,
     medium: `
       font-weight: 400;
-      font-size: 14;
+      font-size: 14px;
       letter-spacing: 0;
     `,
     regular: `
       font-weight: 500;
-      font-size: 12;
+      font-size: 12px;
       letter-spacing: 0;
     `,
     regularAccent: `
       font-weight: 700;
-      font-size: 12;
+      font-size: 12px;
       letter-spacing: 0;
     `,
     small: `
       font-weight: 400;
-      font-size: 10;
+      font-size: 10px;
       letter-spacing: 0;
     `,
     smallAccent: `
       font-weight: 600;
-      font-size: 10;
+      font-size: 10px;
       letter-spacing: 0;
     `,
     extraSmall: `
       font-weight: 400;
-      font-size: 8;
+      font-size: 8px;
       letter-spacing: 0;
     `,
   },
