645725586715a22d81c8ba1276f12c45191f78b8
Seio924 <seio9241@gmail.com>
2025-02-16 21:36:21 +0900

    refactor(#40): ContentInput 스타일 리팩토링

--- src/components/_common/atoms/ContentInput/style.ts
+++ src/components/_common/atoms/ContentInput/style.ts
@@ -3,20 +3,21 @@ import styled from '@emotion/native';
 const ContentInput = styled.TextInput`
   width: 100%;
   min-height: 130px;
-  border: 1px solid #9a9a9a;
+  background-color: ${({ theme }) => theme.color.gray[50]};
+  border: 1px solid ${({ theme }) => theme.color.gray[800]};
   border-radius: 8px;
   padding: 11px 16px;
 `;
 
 const NormalText = styled.Text`
-  font-size: 13px;
-  color: #000;
+  ${({ theme }) => theme.font.common.regular};
+  color: ${({ theme }) => theme.color.gray[900]};
 `;
 
 const HashtagText = styled.Text`
-  font-size: 14px;
-  color: #000;
-  background-color: #caf5da;
+  ${({ theme }) => theme.font.common.regular};
+  color: ${({ theme }) => theme.color.gray[900]};
+  background-color: ${({ theme }) => theme.color.primary[50]};
   border-radius: 5px;
 `;
 
