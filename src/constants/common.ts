924feb7b8185329e370ffae3a26501f5a44a6496
Seio924 <seio9241@gmail.com>
2025-02-27 17:12:25 +0900

    feat(#40): SegmentedControl 컴포넌트 구현 및 상수 정의

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,2 +1,6 @@
 export const DEFAULT_PROFILE_IMAGE =
   'https://friendybucket.s3.us-east-2.amazonaws.com/profile/image.png' as const;
+
+export const PROFILE_VISIBILITY_OPTIONS = ['전체 공개', '비공개'] as const;
+
+export const LOCATION_VISIBILITY_OPTIONS = ['전체', '팔로워', '비공개'] as const;
