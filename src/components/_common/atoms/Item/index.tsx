84c7c3ee2db93efdbb6522a8f6d5b99dac945cf2
Seio924 <seio9241@gmail.com>
2025-02-27 19:26:12 +0900

    feat(#40): Item 컴포넌트 구현

---
+++ src/components/_common/atoms/Item/index.tsx
@@ -0,0 +1,14 @@
+import S, { ItemStyleProps } from './style';
+
+interface ItemProps extends ItemStyleProps {
+  item: string;
+  onPress: () => void;
+}
+
+export default function Item({ item, color = 'default', align = 'center', onPress }: ItemProps) {
+  return (
+    <S.ItemContainer align={align} onPress={onPress}>
+      <S.Text color={color}>{item}</S.Text>
+    </S.ItemContainer>
+  );
+}
