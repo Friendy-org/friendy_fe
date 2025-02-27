573c22be67cef2bf548289ce8cc2e89d01c37f02
Seio924 <seio9241@gmail.com>
2025-02-27 19:26:12 +0900

    feat(#40): Item 컴포넌트 구현

---
+++ src/components/_common/atoms/Item/style.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/native';
+import { Theme } from '@emotion/react';
+
+export type ItemColor = 'default' | 'gray' | 'tertiary';
+export type ItemAlign = 'left' | 'center';
+
+export interface ItemStyleProps {
+  color: ItemColor;
+  align: ItemAlign;
+}
+
+const colorStyles = (theme: Theme, color: ItemColor) => {
+  const colors = {
+    default: `
+      color: ${theme.color.gray[950]};
+    `,
+    gray: `
+      color: ${theme.color.gray[800]};
+    `,
+    tertiary: `
+      color: ${theme.color.tertiary[400]};
+    `,
+  };
+  return colors[color];
+};
+
+const alignStyles = (align: ItemAlign) => `
+  align-items: ${align === 'left' ? 'flex-start' : 'center'};
+`;
+
+const ItemContainer = styled.TouchableOpacity<{ align: ItemAlign }>`
+  display: flex;
+  justify-content: center;
+  ${({ align }) => alignStyles(align)}
+
+  white-space: nowrap;
+  padding: 8px 11px;
+`;
+
+const Text = styled.View<{ color: ItemColor }>`
+  ${({ theme, color }) => colorStyles(theme, color)}
+  ${({ theme }) => theme.font.common.medium}
+`;
+
+const S = {
+  ItemContainer,
+  Text,
+};
+
+export default S;
