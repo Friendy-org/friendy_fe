79d7e861e6dea5d56d3555eb711ee8ad47914c82
Seio924 <seio9241@gmail.com>
2025-02-27 09:49:29 +0900

    refactor(#40): ProfileImage 스타일 리팩토링
    - 기본 이미지 상수 생성

--- src/components/_common/atoms/ProfileImage/style.ts
+++ src/components/_common/atoms/ProfileImage/style.ts
@@ -1,10 +1,38 @@
 import styled from '@emotion/native';
 
-const ProfileImage = styled.Image`
-  width: 104px;
-  height: 104px;
-  border-radius: 104px;
-`;
+type ProfileImageSize = 'x_sm' | 'sm' | 'md' | 'lg';
+
+export interface ProfileImageStyleProps {
+  size: ProfileImageSize;
+}
+
+const sizeStyles = (size: ProfileImageSize) => {
+  const sizes = {
+    x_sm: `
+      width: 32px;
+      height: 32px;
+      border-radius: 16px;
+    `,
+    sm: `
+      width: 40px;
+      height: 40px;
+      border-radius: 20px;
+    `,
+    md: `
+      width: 44px;
+      height: 44px;
+      border-radius: 22px;
+    `,
+    lg: `
+      width: 114px;
+      height: 114px;
+      border-radius: 57px;
+    `,
+  };
+  return sizes[size];
+};
+
+const ProfileImage = styled.Image<ProfileImageStyleProps>(({ size }) => `${sizeStyles(size)}`);
 
 const S = {
   ProfileImage,
---
+++ src/constants/common.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_PROFILE_IMAGE =
+  'https://friendybucket.s3.us-east-2.amazonaws.com/profile/image.png' as const;
