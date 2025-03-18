6cef502a95122dda1c8f25622d21c9654ce6fd07
Seio924 <seio9241@gmail.com>
2025-03-18 22:16:20 +0900

    refactor(#42): SetProfile 분리

---
+++ src/screens/SignUpScreen/SetProfile/style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/native';
+
+const Container = styled.View`
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const InnerForm = styled.View`
+  display: flex;
+  gap: 18px;
+
+  padding: 40px 0;
+`;
+
+const S = {
+  Container,
+  InnerForm,
+};
+
+export default S;
--- src/screens/SignUpScreen/style.ts
+++ src/screens/SignUpScreen/style.ts
@@ -1,37 +1,17 @@
 import styled from '@emotion/native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-const Layout = styled.View`
+const Layout = styled(ScrollView)`
+  display: flex;
+  flex-grow: 1;
+
   width: 100%;
   height: 100%;
   padding: 0px 40px;
-  background-color: white;
-`;
-
-const SignUpContainer = styled(ScrollView)``;
-
-const ProfileSettingsContainer = styled.View`
-  display: flex;
-  flex-grow: 1;
-  justify-content: space-between;
-`;
-
-const InnerForm = styled.View`
-  display: flex;
-  padding: 40px 0;
-`;
-
-const IndicatorContainer = styled.View`
-  padding: 10px 0 10px 0;
-  margin-bottom: 40px;
 `;
 
 const S = {
   Layout,
-  SignUpContainer,
-  ProfileSettingsContainer,
-  InnerForm,
-  IndicatorContainer,
 };
 
 export default S;
