24bb5cb4a8186bfeea57f1a8da9064bcc5b20a33
Seio924 <seio9241@gmail.com>
2025-03-18 21:37:49 +0900

    refactor(#42): 에러메시지 상수 리팩토링

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,13 +1,11 @@
 export const FORM_ERROR = {
   EMAIL: {
-    REQUIRED: '이메일이 입력되지 않았습니다.',
+    REQUIRED: '이메일을 입력해 주세요.',
     INVALID: '이메일 형식으로 입력해주세요.',
-    ALREADY_EXISTS: '이미 가입된 이메일입니다.',
   },
   NICKNAME: {
-    REQUIRED: '닉네임이 입력되지 않았습니다.',
+    REQUIRED: '닉네임을 입력해 주세요.',
     LENGTH: '닉네임은 2~20자 사이로 입력해주세요.',
-    ALREADY_EXISTS: '닉네임이 이미 존재합니다.',
   },
   PASSWORD: {
     REQUIRED: '비밀번호가 입력되지 않았습니다.',
