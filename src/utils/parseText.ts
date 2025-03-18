4a9cf01215e997d354e16a9ee5b0626c83b0d397
Seio924 <seio9241@gmail.com>
2025-03-18 21:38:11 +0900

    refactor(#42): 해시태그 추출 함수 리팩토링

--- src/utils/parseText.ts
+++ src/utils/parseText.ts
@@ -1,25 +1,29 @@
-export interface ParsedText {
+import { v4 as uuidv4 } from 'uuid';
+
+interface ExtractHashtagText {
+  id: string;
   str: string;
   isHashtag: boolean;
-  idxArr: number[];
+  startIdx: number;
+  endIdx: number;
 }
 
-export const extractHashtags = (text: string): ParsedText[] => {
-  if (text.length === 0) {
-    return [];
-  }
-
+export const extractHashtags = (text: string): ExtractHashtagText[] => {
+  const parsedText: ExtractHashtagText[] = [];
   const words = text.split(' ');
-  let index = 0;
 
-  return words.map(word => {
-    const idxArr = [index, index + word.length - 1];
-    index += word.length + 1;
+  words.forEach((word, index) => {
+    const startIdx = index === 0 ? 0 : parsedText[index - 1].endIdx + 2;
+    const endIdx = startIdx + word.length - 1;
 
-    return {
+    parsedText.push({
+      id: uuidv4(),
       str: word,
       isHashtag: word.startsWith('#'),
-      idxArr,
-    };
+      startIdx,
+      endIdx,
+    });
   });
+
+  return parsedText;
 };
