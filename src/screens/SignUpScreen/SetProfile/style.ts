d7b290e5693696b8445655f6338cec7e9c481bd8
Seio924 <seio9241@gmail.com>
2025-03-19 14:36:55 +0900

    refactor(#42): SignUpScreen 리팩토링 완료

--- src/screens/SignUpScreen/SetProfile/style.ts
+++ src/screens/SignUpScreen/SetProfile/style.ts
@@ -2,20 +2,25 @@ import styled from '@emotion/native';
 
 const Container = styled.View`
   display: flex;
+  flex: 1;
+  gap: 40px;
+`;
 
-  width: 100%;
-  height: 100%;
+const SetProfileForm = styled.View`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  gap: 40px;
 `;
 
 const InputForm = styled.View`
   display: flex;
   gap: 18px;
-
-  padding: 40px 0;
 `;
 
 const S = {
   Container,
+  SetProfileForm,
   InputForm,
 };
 
