3ebbfdf4b13a82b7c305bc622b34c0511758a627
Seio924 <seio9241@gmail.com>
2025-03-21 18:14:02 +0900

    feat(#42): 비밀번호 변경 api 핸들러 추가

--- src/screens/ResetPasswordScreen/SetPassword/index.tsx
+++ src/screens/ResetPasswordScreen/SetPassword/index.tsx
@@ -1,8 +1,6 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-
 import { FormRegister } from '@customTypes/common';
-import { RootStackParamList } from '@customTypes/navigation';
+
+import useResetPassword from '@hooks/member/useResetPassword';
 
 import Button from '@components/_common/atoms/Button';
 import InputField from '@components/_common/molecules/InputField';
@@ -10,23 +8,22 @@ import InputField from '@components/_common/molecules/InputField';
 import S from './style';
 
 interface SetPasswordProps<TFieldData> {
+  email: string;
   passwordRegister: FormRegister;
   confirmPasswordRegister: FormRegister;
   handlePress: (callback: () => Promise<void>, excludeFields?: (keyof TFieldData)[]) => () => void;
 }
 
-type NavigationProp = StackNavigationProp<RootStackParamList>;
-
 export default function SetPassword<TFieldData>({
+  email,
   passwordRegister,
   confirmPasswordRegister,
   handlePress,
 }: SetPasswordProps<TFieldData>) {
-  const navigation = useNavigation<NavigationProp>();
+  const { resetPasswordMutate } = useResetPassword();
 
   const handleResetPassword = async () => {
-    // Todo: 비밀번호 재설정 API
-    navigation.goBack();
+    resetPasswordMutate.mutate({ email, newPassword: passwordRegister.value });
   };
 
   return (
