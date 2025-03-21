d6ede84dbb2ded42689685206d0fcadf5bfb08af
Seio924 <seio9241@gmail.com>
2025-03-21 17:55:05 +0900

    refactor(#42): 회원가입 api 핸들러 리팩토링

--- src/screens/SignUpScreen/SetProfile/index.tsx
+++ src/screens/SignUpScreen/SetProfile/index.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 
 import { FormRegister } from '@customTypes/common';
 
+import useSignup from '@hooks/member/useSignup';
+
+import { DEFAULT_PROFILE_IMAGE } from '@constants/common';
+
 import UserProfile from '@components/_common/molecules/UserProfile';
 import InputField from '@components/_common/molecules/InputField';
 import DateInputField from '@components/_common/molecules/DateInputField';
@@ -10,6 +14,7 @@ import Button from '@components/_common/atoms/Button';
 import S from './style';
 
 interface SetProfileProps<TFieldData> {
+  email: string;
   nicknameRegister: FormRegister;
   passwordRegister: FormRegister;
   confirmPasswordRegister: FormRegister;
@@ -17,19 +22,34 @@ interface SetProfileProps<TFieldData> {
 }
 
 export default function SetProfile<TFieldData>({
+  email,
   nicknameRegister,
   passwordRegister,
   confirmPasswordRegister,
   handlePress,
 }: SetProfileProps<TFieldData>) {
+  const { signupMutate } = useSignup();
   const [date, setDate] = useState(new Date());
+  const [imageUrl, setImageUrl] = useState<string>(DEFAULT_PROFILE_IMAGE);
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async () => {
+    signupMutate.mutate({
+      email,
+      nickname: nicknameRegister.value,
+      password: passwordRegister.value,
+      birthDate: date.toISOString().split('T')[0],
+      imageUrl,
+    });
+  };
 
   return (
     <S.Container>
       <S.SetProfileForm>
-        <UserProfile text='프로필 사진 선택' />
+        <UserProfile
+          text='프로필 사진 선택'
+          imageUrl={imageUrl}
+          onSelectImage={setImageUrl}
+        />
 
         <S.InputForm>
           <InputField
