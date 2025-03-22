007e9a0262cf1cdd81027b3842576d75621a388c
Seio924 <seio9241@gmail.com>
2025-03-23 01:41:35 +0900

    feat(#42): 로딩 컴포넌트 구현

---
+++ src/screens/Loading/style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/native';
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+const S = {
+  Container,
+};
+
+export default S;
