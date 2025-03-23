e4459a5a143c8afd5273e2eb444b8ffbe1972357
Seio924 <seio9241@gmail.com>
2025-03-23 11:41:52 +0900

    refactor(#42): ImageUploader 컴포넌트 분리

--- src/components/ImageUploader/AddImage/style.ts
+++ src/components/ImageUploader/AddImage/style.ts
@@ -1,16 +1,5 @@
 import styled from '@emotion/native';
 
-const ImageWrapper = styled.View`
-  position: relative;
-  margin-right: 10px;
-`;
-
-const CloseButton = styled.TouchableOpacity`
-  position: absolute;
-  top: 5px;
-  right: 5px;
-`;
-
 const AddImageButton = styled.TouchableOpacity`
   display: flex;
   align-items: center;
@@ -18,14 +7,13 @@ const AddImageButton = styled.TouchableOpacity`
 
   width: 150px;
   height: 150px;
+
   background-color: ${({ theme }) => theme.color.gray[50]};
   border: 1px ${({ theme }) => theme.color.gray[700]} dashed;
   border-radius: 8px;
 `;
 
 const S = {
-  ImageWrapper,
-  CloseButton,
   AddImageButton,
 };
 
