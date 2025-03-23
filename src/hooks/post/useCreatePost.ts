0486d3e3142a7d24011e0be0c71f2abc83077fb4
Seio924 <seio9241@gmail.com>
2025-03-23 11:58:13 +0900

    feat(#42): 게시글 생성 훅 구현

---
+++ src/hooks/post/useCreatePost.ts
@@ -0,0 +1,24 @@
+import { useNavigation } from '@react-navigation/native';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import postApi from '@api/domain/post';
+
+import { STACK_NAME } from '@constants/navigation';
+import QUERY_KEYS from '@constants/queryKeys';
+
+import { NavigationProp } from '@customTypes/navigation';
+
+export default function useCreatePost() {
+  const navigation = useNavigation<NavigationProp>();
+  const queryClient = useQueryClient();
+
+  const createPostMutate = useMutation({
+    mutationFn: postApi.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.POST_LIST] });
+      navigation.replace(STACK_NAME.FEED);
+    },
+  });
+
+  return { createPostMutate };
+}
