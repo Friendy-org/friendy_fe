eddd455a25ac03ec4f1ab65185b4ff124d4e35b5
Seio924 <seio9241@gmail.com>
2025-03-23 11:58:29 +0900

    feat(#42): 게시글 생성 mock 설정

---
+++ src/mocks/data/post/createPostData.ts
@@ -0,0 +1,4 @@
+export const createPostResponse = {
+  code: 1000,
+  message: '게시글 생성에 성공했습니다.',
+};
--- src/mocks/handlers/post.ts
+++ src/mocks/handlers/post.ts
@@ -1,7 +1,10 @@
 import { END_POINTS } from '@constants/api';
 import type MockAdapter from 'axios-mock-adapter';
 import { getPostListResponse } from '../data/post/getPostListData';
+import { createPostResponse } from '../data/post/createPostData';
 
 export default function postHandler(mock: MockAdapter) {
   mock.onGet(END_POINTS.POST.LIST).reply(200, getPostListResponse);
+
+  mock.onPost(END_POINTS.POST.CREATE).reply(200, createPostResponse);
 }
