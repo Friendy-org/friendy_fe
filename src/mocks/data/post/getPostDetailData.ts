c7973225b8f9106aab1cf4fe46a9bc55928d2d77
Seio924 <seio9241@gmail.com>
2025-03-25 22:02:00 +0900

    feat(#42): 게시글 상세 조회 mock 추가

---
+++ src/mocks/data/post/getPostDetailData.ts
@@ -0,0 +1,25 @@
+export const getPostDetailResponse = {
+  code: 1000,
+  message: '게시글 상세 조회에 성공했습니다.',
+  result: {
+    me: true,
+    id: 1,
+    content: '프렌디게시글내용',
+    location: '경남 창원시',
+    createdAt: '2025-03-19T09:13:47',
+    likeCount: 0,
+    commentCount: 0,
+    shareCount: 0,
+    authorResponse: {
+      id: 1,
+      nickname: 'bokSungKim',
+      profileImageUrl:
+        'https://i.namu.wiki/i/pcuapOq_pmNJ-l3XnG1-5y-FawoBIe9NV6Xs8n8s4l9NxmbdzN34XJxhpm1iy6uWMK2MMcxPtD9_S3Wv1HGQxw.webp',
+    },
+    imageUrls: [
+      'https://www.adobe.com/kr/creativecloud/photography/hub/features/roc/media_126f51234e424100112aecb4f61e26b3a2afc74d8.jpeg?width=750&format=jpeg&optimize=medium',
+      'https://media.istockphoto.com/id/520700958/ko/%EC%82%AC%EC%A7%84/%EC%95%84%EB%A6%84%EB%8B%A4%EC%9A%B4-%EA%BD%83-%EB%B0%B0%EA%B2%BD%EA%B8%B0%EC%88%A0.jpg?s=612x612&w=0&k=20&c=gJx5-O9U1qXKZqKwv4KunrBae7RDNRcdse1nOdSk_0w=',
+      'https://media.istockphoto.com/id/157681198/ko/%EC%82%AC%EC%A7%84/%EB%AF%BC%EB%93%A4%EB%A0%88-seed.jpg?s=612x612&w=0&k=20&c=BtRA1Q24ICmHkRb6HS0tInJ1JQKAEiTm0dC1tBtfs5s=',
+    ],
+  },
+};
