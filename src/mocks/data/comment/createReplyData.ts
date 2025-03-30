6f3ceebd7b7f8a9442264777ac994197803f368a
Seio924 <seio9241@gmail.com>
2025-03-30 22:25:48 +0900

    feat(#46): 답글 생성 mock 구현

---
+++ src/mocks/data/comment/createReplyData.ts
@@ -0,0 +1,4 @@
+export const createReplyResponse = {
+  code: 1000,
+  message: '답글 생성에 성공했습니다.',
+};
--- src/mocks/handlers/comment.ts
+++ src/mocks/handlers/comment.ts
@@ -4,6 +4,7 @@ import { pathToRegex } from '../utils/mockApiHelper';
 import { getCommentListResponse } from '../data/comment/getCommentListData';
 import { getReplyListResponse } from '../data/comment/getReplyListData';
 import { createCommentResponse } from '../data/comment/createCommentData';
+import { createReplyResponse } from '../data/comment/createReplyData';
 
 export default function commentHandler(mock: MockAdapter) {
   mock.onGet(pathToRegex(MOCK_ENDPOINTS.COMMENT.LIST)).reply(200, getCommentListResponse);
@@ -11,4 +12,6 @@ export default function commentHandler(mock: MockAdapter) {
   mock.onGet(pathToRegex(MOCK_ENDPOINTS.COMMENT.REPLY_LIST)).reply(200, getReplyListResponse);
 
   mock.onPost(MOCK_ENDPOINTS.COMMENT.CREATE).reply(200, createCommentResponse);
+
+  mock.onPost(MOCK_ENDPOINTS.COMMENT.REPLY_CREATE).reply(200, createReplyResponse);
 }
