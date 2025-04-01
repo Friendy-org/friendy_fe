086a01d1db543421e990f44a24a419f52defdb15
Seio924 <seio9241@gmail.com>
2025-04-01 16:47:13 +0900

    feat(#48): 게시글 수정 훅 구현

---
+++ src/hooks/post/useUpdatePost.ts
@@ -0,0 +1,26 @@
+import { useNavigation } from '@react-navigation/native';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import postApi from '@api/domain/post';
+
+import { STACK_NAME } from '@constants/navigation';
+import QUERY_KEYS from '@constants/queryKeys';
+
+import { NavigationProp } from '@customTypes/navigation';
+import { PostFormData } from '@customTypes/post';
+
+export default function useUpdatePost() {
+  const navigation = useNavigation<NavigationProp>();
+  const queryClient = useQueryClient();
+
+  const updatePostMutate = useMutation({
+    mutationFn: ({ postId, editPostData }: { postId: string; editPostData: PostFormData }) =>
+      postApi.update(postId, editPostData),
+    onSuccess: ({ id }: { id: string }) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.POST_DETAIL, id] });
+      navigation.replace(STACK_NAME.FEED_DETAIL, { postId: Number(id) });
+    },
+  });
+
+  return { updatePostMutate };
+}
