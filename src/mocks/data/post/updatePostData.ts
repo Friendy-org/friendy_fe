b77c6b33f9e7a3fafdbc64ff26c55052480673e8
Seio924 <seio9241@gmail.com>
2025-04-01 16:47:32 +0900

    feat(#48): 게시글 수정 mock 추가

---
+++ src/mocks/data/post/updatePostData.ts
@@ -0,0 +1,7 @@
+export const updatePostResponse = {
+  code: 1402,
+  message: '게시글 수정에 성공했습니다.',
+  result: {
+    id: 1,
+  },
+};
--- src/mocks/handlers/post.ts
+++ src/mocks/handlers/post.ts
@@ -4,6 +4,7 @@ import { createPostResponse } from '../data/post/createPostData';
 import { getPostDetailResponse } from '../data/post/getPostDetailData';
 import { MOCK_ENDPOINTS } from '../mockEndpoints';
 import { pathToRegex } from '../utils/mockApiHelper';
+import { updatePostResponse } from '../data/post/updatePostData';
 
 export default function postHandler(mock: MockAdapter) {
   mock.onGet(MOCK_ENDPOINTS.POST.LIST).reply(200, getPostListResponse);
@@ -11,4 +12,6 @@ export default function postHandler(mock: MockAdapter) {
   mock.onGet(pathToRegex(MOCK_ENDPOINTS.POST.DETAIL)).reply(200, getPostDetailResponse);
 
   mock.onPost(MOCK_ENDPOINTS.POST.CREATE).reply(200, createPostResponse);
+
+  mock.onPost(MOCK_ENDPOINTS.POST.UPDATE).reply(200, updatePostResponse);
 }
