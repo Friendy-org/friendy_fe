905245d4879899a2166e8d7bddf7a2929e10cf91
Seio924 <seio9241@gmail.com>
2025-04-05 18:46:38 +0900

    feat(#49): 사용자 정보 조회 mock 추가

---
+++ src/mocks/data/member/getMemberInfoData.ts
@@ -0,0 +1,13 @@
+export const getMemberInfoResponse = {
+  code: 1000,
+  message: '사용자 정보 조회에 성공했습니다.',
+  result: {
+    me: true,
+    email: 'example@friendy.com',
+    nickname: 'bokSungKim',
+    profileImageUrl:
+      'https://i.namu.wiki/i/pcuapOq_pmNJ-l3XnG1-5y-FawoBIe9NV6Xs8n8s4l9NxmbdzN34XJxhpm1iy6uWMK2MMcxPtD9_S3Wv1HGQxw.webp',
+    followerCount: 0,
+    followingCount: 0,
+  },
+};
