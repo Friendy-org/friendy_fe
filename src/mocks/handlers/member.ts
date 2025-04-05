89764edeb495f75d00e37d4a5457b8f1847de1df
Seio924 <seio9241@gmail.com>
2025-04-05 18:46:38 +0900

    feat(#49): 사용자 정보 조회 mock 추가

--- src/mocks/handlers/member.ts
+++ src/mocks/handlers/member.ts
@@ -2,8 +2,12 @@ import type MockAdapter from 'axios-mock-adapter';
 import { signupResponse } from '../data/member/signupData';
 import { resetPasswordResponse } from '../data/member/resetPasswordData';
 import { MOCK_ENDPOINTS } from '../mockEndpoints';
+import { pathToRegex } from '../utils/mockApiHelper';
+import { getMemberInfoResponse } from '../data/member/getMemberInfoData';
 
 export default function memberHandler(mock: MockAdapter) {
+  mock.onGet(pathToRegex(MOCK_ENDPOINTS.MEMBER.INFO)).reply(200, getMemberInfoResponse);
+
   mock.onPost(MOCK_ENDPOINTS.MEMBER.SIGNUP).reply(200, signupResponse);
 
   mock.onPost(MOCK_ENDPOINTS.MEMBER.RESET_PASSWORD).reply(200, resetPasswordResponse);
