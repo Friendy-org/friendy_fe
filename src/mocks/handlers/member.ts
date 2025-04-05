c4d1fa5517f2079c3e4712a412dbd9a82e95cfb2
Seio924 <seio9241@gmail.com>
2025-04-05 20:46:56 +0900

    feat(#49): 내가 작성한 게시글 목록 조회 mock 추가

--- src/mocks/handlers/member.ts
+++ src/mocks/handlers/member.ts
@@ -4,9 +4,12 @@ import { resetPasswordResponse } from '../data/member/resetPasswordData';
 import { MOCK_ENDPOINTS } from '../mockEndpoints';
 import { pathToRegex } from '../utils/mockApiHelper';
 import { getMemberInfoResponse } from '../data/member/getMemberInfoData';
+import { getMyPostListResponse } from '../data/member/getMyPostListData';
 
 export default function memberHandler(mock: MockAdapter) {
   mock.onGet(pathToRegex(MOCK_ENDPOINTS.MEMBER.INFO)).reply(200, getMemberInfoResponse);
+
+  mock.onGet(pathToRegex(MOCK_ENDPOINTS.MEMBER.MY_POST_LIST)).reply(200, getMyPostListResponse);
 
   mock.onPost(MOCK_ENDPOINTS.MEMBER.SIGNUP).reply(200, signupResponse);
 
