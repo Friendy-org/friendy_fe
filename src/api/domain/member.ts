76758b0e0d92871ebad6457b5b03ddf637bc2abd
Seio924 <seio9241@gmail.com>
2025-04-05 18:44:41 +0900

    feat(#49): 인터셉터에 멤버ID 제거 추가

--- src/api/domain/member.ts
+++ src/api/domain/member.ts
@@ -3,6 +3,12 @@ import { END_POINTS } from '@constants/api';
 import { ResetPasswordData, SignupData } from '@customTypes/member';
 
 const memberApi = {
+  getInfo: async (memberId: string) => {
+    const { data } = await axiosInstance.get(END_POINTS.MEMBER.INFO(memberId));
+
+    return data.result;
+  },
+
   signup: async (signupData: SignupData) => {
     const { data } = await axiosInstance.post(END_POINTS.MEMBER.SIGNUP, signupData, {
       useAuth: false,
@@ -15,12 +21,6 @@ const memberApi = {
     await axiosInstance.post(END_POINTS.MEMBER.RESET_PASSWORD, resetPasswordData, {
       useAuth: false,
     });
-  },
-
-  getInfo: async (memberId: string) => {
-    const { data } = await axiosInstance.get(END_POINTS.MEMBER.INFO(memberId));
-
-    return data.result;
   },
 };
 
